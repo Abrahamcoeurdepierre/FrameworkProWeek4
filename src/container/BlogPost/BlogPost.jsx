8e6734bd5e58f689f130914dac5e5f18c9573a5b
Abrahamcoeurdepierre <letmeloveyounow27@gmail.com>
2021-03-02 20:00:20 -0800

    Rakotoaritsoa Fenohasina

---
+++ src/container/BlogPost/BlogPost.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import './BlogPost.css'
+import Post from './Post'
+
+class BlogPost extends Component {
+
+    state = {
+        listArtikel: []
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(response => response.json())
+        .then(jsonHasilAmbilDariAPI =>{
+            this.setState({
+                listArtikel: jsonHasilAmbilDariAPI
+            })
+        })
+    }
+    render() {
+        return (
+            <div>
+                <div className="post-artikel">
+                    <h2>Daftar Artikel</h2>
+                    {
+                        this.state.listArtikel.map(artikel => {
+                            return <Post key = {artikel.id} judul={artikel.title} isi = {artikel.body}/>
+                        })
+                    }
+                </div>
+            </div>
+        )
+    }
+}
+
+export default BlogPost;
